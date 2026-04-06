a10d3f738ed8dfe1137baea94312c48d108f69ef
stemmqdevteam <stemmqdevs@gmail.com>
2026-04-06 15:21:39 +0100

    latest update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,13 +5,6 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  {
-    rules: {
-      "react/no-unescaped-entities": "off",
-      "react-hooks/set-state-in-effect": "off",
-      "@typescript-eslint/no-explicit-any": "off",
-    },
-  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
